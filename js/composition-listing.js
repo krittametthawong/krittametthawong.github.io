0866ae6c2c2bbae18a00e8ec56faa6e99bf75e84
Krittamet Thawong <37761388+timor2542@users.noreply.github.com>
2021-07-06 18:09:08 +0700

    Comitted 6/7/2564 18:09

---
+++ js/composition-listing.js
@@ -0,0 +1,63 @@
+(function() {
+  var template = function(data) {
+    return '<div class="widget'+ (data.isNew?' widget--new':'') +'">'+
+        '<img src="http://widgetic.com' + data.previews.small + '"/>'+
+        '<span class="widget_name">'+ data.name + '</span>' +
+      '</div>';
+  }
+
+  var CompositionListing = function(el) {
+    this.el = el;
+
+    this.el.on('click', 'li', this._onCompositionClick.bind(this))
+
+    this.el.html('Select a widget to list the available compositions!');
+  }
+
+  CompositionListing.EDIT = 0;
+  CompositionListing.NEW  = 1;
+
+  CompositionListing.prototype.load = function(widget) {
+    this.widget = widget;
+
+    return Widgetic.api('compositions', {widget_id: widget.id})
+      .then(this._onCompositionsLoaded.bind(this))
+  };
+
+  CompositionListing.prototype.refresh = function() {
+    this.load(this.widget);
+  }
+
+  CompositionListing.prototype._onCompositionsLoaded = function(compositions) {
+    this.el.empty()
+    
+    compositions.map(function(composition) {
+      var item = $('<li>')
+        .html(template({previews: this.widget.previews, name: composition.name}))
+        .data('composition', composition)
+      this.el.append(item)
+    }.bind(this))
+
+    var item = $('<li>')
+      .html(template({isNew: true, previews: this.widget.previews, name: 'Create new composition'}))
+      .data('widget', this.widget)
+    this.el.append(item)
+  }
+
+  CompositionListing.prototype._onCompositionClick = function(ev) {
+    var el = $(ev.currentTarget);
+    var composition = el.data('composition');
+    
+    el.addClass('selected')
+      .siblings()
+      .removeClass('selected');
+
+    if(composition) {
+      this.el.trigger('composition-selected', { composition: composition, type: CompositionListing.EDIT });
+    } else {
+      this.el.trigger('composition-selected', { widget: $(ev.currentTarget).data('widget'), type: CompositionListing.NEW });
+    }
+  };
+
+  window.CompositionListing = CompositionListing;
+}());
